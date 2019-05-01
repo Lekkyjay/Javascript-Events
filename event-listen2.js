fd65e588cdc86439d8f2ae4f7de2128fef47698a
Lekkyjay <lekkyjayjay@yahoo.com>
2019-05-01 21:57:32 +0200

    Moved the script tag into the html head section

---
+++ event-listen2.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+  let btn = document.getElementById('btn');
+  let lnk = document.getElementById('lnk');
+  let txt = document.getElementById('txt');
+
+  btn.addEventListener('click', (ev) => {
+    console.log(ev.type, ev.target, ev.currentTarget);
+  });
+  
+  lnk.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    console.log(ev.type, ev.target, ev.currentTarget);
+  });
+  
+  txt.addEventListener('input', (ev) => {
+    console.log(ev.type, ev.target, ev.target.value);
+  });
+  
+  txt.addEventListener('change', (ev) => {
+    console.log(ev.type, ev.target, ev.target.value);
+  });
+  
+  txt.addEventListener('blur', (ev) => {
+    console.log(ev.type, ev.target, ev.target.value);
+  });
+}
