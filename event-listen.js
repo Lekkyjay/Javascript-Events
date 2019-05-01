a2f772b2fa78770bf619bd05321f3239ddfd990e
Lekkyjay <lekkyjayjay@yahoo.com>
2019-05-01 20:25:24 +0200

    Using anonymous functions as event handlers

--- event-listen.js
+++ event-listen.js
@@ -2,20 +2,23 @@ let btn = document.getElementById('btn');
 let lnk = document.getElementById('lnk');
 let txt = document.getElementById('txt');
 
-btn.addEventListener('click', buttonClicked);
-function buttonClicked(ev) {
+btn.addEventListener('click', function(ev) {
   console.log(ev.type, ev.target, ev.currentTarget);
-}
+});
 
-
-lnk.addEventListener('click', linkClicked);
-function linkClicked(ev) {
+lnk.addEventListener('click', function(ev) {
   ev.preventDefault();
   console.log(ev.type, ev.target, ev.currentTarget);
-}
+});
 
+txt.addEventListener('input', function(ev) {
+  console.log(ev.type, ev.target, ev.target.value);
+});
 
-txt.addEventListener('input', inputMade);
-function inputMade(ev) {
-  console.log(ev.type, ev.target, ev.currentTarget);
-}
+txt.addEventListener('change', function(ev) {
+  console.log(ev.type, ev.target, ev.target.value);
+});
+
+txt.addEventListener('blur', function(ev) {
+  console.log(ev.type, ev.target, ev.target.value);
+});
