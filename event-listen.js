258e1a4b04bbb7568d19451e0e6b7df5ce3bad5d
Lekkyjay <lekkyjayjay@yahoo.com>
2019-05-01 20:28:16 +0200

    Using arrow functions as event handlers

--- event-listen.js
+++ event-listen.js
@@ -2,23 +2,23 @@ let btn = document.getElementById('btn');
 let lnk = document.getElementById('lnk');
 let txt = document.getElementById('txt');
 
-btn.addEventListener('click', function(ev) {
+btn.addEventListener('click', (ev) => {
   console.log(ev.type, ev.target, ev.currentTarget);
 });
 
-lnk.addEventListener('click', function(ev) {
+lnk.addEventListener('click', (ev) => {
   ev.preventDefault();
   console.log(ev.type, ev.target, ev.currentTarget);
 });
 
-txt.addEventListener('input', function(ev) {
+txt.addEventListener('input', (ev) => {
   console.log(ev.type, ev.target, ev.target.value);
 });
 
-txt.addEventListener('change', function(ev) {
+txt.addEventListener('change', (ev) => {
   console.log(ev.type, ev.target, ev.target.value);
 });
 
-txt.addEventListener('blur', function(ev) {
+txt.addEventListener('blur', (ev) => {
   console.log(ev.type, ev.target, ev.target.value);
 });
